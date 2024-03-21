7d688658db3fc3eb6dfad1163dff2e1474fb78ed
krepovsepo <164184295+krepovsepo@users.noreply.github.com>
2024-03-21 12:25:35 +0100

    Add files via upload

--- matrix-script.sh
+++ matrix-script.sh
@@ -140,7 +140,8 @@ server {
 
 	add_header X-Frame-Options SAMEORIGIN;
 	add_header X-Content-Type-Options nosniff;
-	add_header X-XSS-Protection "1; mode=block";
+	add_header X-XSS-Protection \"1; mode=block\";
+	add_header Content-Security-Policy \"frame-ancestors 'self'\";
 
         location / {
                 proxy_pass https://localhost:8088;
